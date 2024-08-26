67dfdab675cede766b8a2d58f13d71b0850b4bd8
Nahuel Bocanegra <nahuboca7@gmail.com>
2024-08-26 14:21:47 -0300

    rutas pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
-
+import {BrowserRouter as Router , Link , Route, Routes} from "react-router-dom"
 import './App.css'
+import axios from "axios"
+import { useState, useEffect } from "react"
+    
+import Nav from "./component/Nav"
+import Boars from "./pages/Boats"
+import Characters from "./pages/Characters"
+import Episodes from "./pages/Episodes"
+import Fruits from "./pages/Fruits"
+import Locations from "./pages/Locations"
+    
+
+
 
 function App() {
-  
+  const [one, setOne]=useState([]);
+  useEffect(()=>{
+    axios.get('https://api.api-onepiece.com/v2/crews/en')
+    .then((res)=>{setOne(res.data)})
+    .catch(((error)=>{console.log(error)}))
+  },[])
+  console.log(one)
+
   return (
-    <>
-    
+      <>
+      <Nav></Nav>
+      <Router>
+        <Routes>
+          <Route path="/"/>
+          <Route path="/home/Boars" element={<Boars></Boars>} />
+          <Route path="/home/Characters" element={<Characters></Characters>} />
+          <Route path="/home/Episodes" element={<Episodes></Episodes>} />
+          <Route path="/home/Fruits" element={<Fruits></Fruits>} />
+          <Route path="/home/Locations" element={<Locations></Locations>} />
+        </Routes>
+      </Router>
     </>
   )
 }
